76098e3d8f903407d5c3f0db0a325c4b247f9f63
Wrexr <48512320+Wrexr-xyz@users.noreply.github.com>
2024-10-03 12:02:03 +0000

    Oct 3, 2024, 12:02 PM

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-500">
-
+    <div class="flex justify-center items-start my-4">
+        <h1 class="text-3xl font-medium">Laravel01 - Test</h1>
+    </div>
 </body>
 </html>
