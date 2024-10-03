26dfca33e6d007da93f96bd4ec33574f84014763
Wrexr <48512320+Wrexr-xyz@users.noreply.github.com>
2024-10-03 07:48:31 +0000

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel01.wrexr.com</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
